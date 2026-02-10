dc1fe7a70242527fa2b2f5964ceaaa4294615533
DnBright <dnbright000@gmail.com>
2026-02-10 16:55:58 +0700

    update

--- portfolio/src/app/not-found.tsx
+++ portfolio/src/app/not-found.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import Button from '@/components/ui/Button';
 import { FaExclamationTriangle, FaTerminal } from 'react-icons/fa';
--- portfolio/src/components/layout/Footer.tsx
+++ portfolio/src/components/layout/Footer.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
 
