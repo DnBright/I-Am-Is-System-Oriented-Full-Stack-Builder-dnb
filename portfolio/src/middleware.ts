0d954dcd12b7f9f72c61b666e92f67a829a4c2bd
DnBright <dnbright000@gmail.com>
2026-02-16 18:53:59 +0700

    “update”

--- portfolio/src/middleware.ts
+++ portfolio/src/middleware.ts
@@ -4,6 +4,6 @@ import { routing } from './navigation';
 export default createMiddleware(routing);
 
 export const config = {
-    // Match only internationalized pathnames
-    matcher: ['/', '/(id|en)/:path*', '/((?!api|_next|_vercel|.*\\..*).*)']
+    // Matcher excluding common static files and API routes
+    matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
